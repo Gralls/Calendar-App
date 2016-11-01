7fa4bec6d0d45e8ad135c244d1c5198424130673
Radoslaw Wojtysiak <worad94@gmail.com>
2016-11-01 22:10:20 +0100

    Authenticate + Register via api

--- server.js
+++ server.js
@@ -2,19 +2,29 @@
 
 var express    = require('express');        
 var app        = express();   
-
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var passport	= require('passport');
+var jwt         = require('jwt-simple');
+
+var config      = require('./config/database'); // get db config file
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://tas_admin:tas123@ds021356.mlab.com:21356/tas-project');
+mongoose.connect(config.database);
+
+require('./config/passport')(passport);
+
 
 var Task = require('./models/task');
 var User = require('./models/user');
 
 var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
+
+app.use(passport.initialize());
 
 app.use('/', express.static(__dirname));
 app.get('/', function(req, res) {
@@ -25,6 +35,69 @@ router.use(function(req,res,next){
 	console.log('Request');
 	next();
 });
+
+
+//----------------------------------------------------------------------------------------
+
+
+router.route('/auth')
+	.post(function(req, res){
+	  User.findOne({
+	    login: req.body.login
+	  }, function(err, user) {
+	    if (err) throw err;
+	 
+	    if (!user) {
+	      res.send({success: false, msg: 'Authentication failed. User not found.'});
+	    } else {
+	      // check if password matches
+	      user.comparePassword(req.body.password, function (err, isMatch) {
+	        if (isMatch && !err) {
+	          // if user is found and password is right create a token
+	          var token = jwt.encode(user, config.secret);
+	          // return the information including token as JSON
+	          res.json({success: true, token: 'JWT ' + token});
+	        } else {
+	          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+	        }
+	      });
+	    }
+	  });
+});
+
+router.route('/checkauth')
+	.get(passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+    User.findOne({
+      login: decoded.login
+    }, function(err, user) {
+        if (err) throw err;
+ 
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+          res.json({success: true, msg: 'Welcome in the member area ' + user.login + '!'});
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+ 
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 
 //----------------------------------------------------------------------------------------
 
@@ -109,17 +182,25 @@ router.route('/users')
 		});
 	})
 	.post(function(req,res){
-		var user = new User();
-		user.name = req.body.name;
-		user.login = req.body.login;
-		user.email = req.body.email;
-		user.password = req.body.password;
-		user.save(function(err){
-			if(err)
-				res.send(err);
-			res.json({message:'User created!'});
+		if (!req.body.login || !req.body.password) {
+		    res.json({success: false, msg: 'Please pass name and password.'});
+		  } else {
+		    var newUser = new User({
+  		      name: req.body.name,
+		      login: req.body.login,
+		      password: req.body.password,
+		      email: req.body.email
+		    });
+		    // save the user
+		    newUser.save(function(err) {
+		      if (err) {
+		        return res.json({success: false, msg: 'Username already exists.'});
+		      }
+		      res.json({success: true, msg: 'Successful created new user.'});
+		    });
+		  }
 		});
-	});
+
 
 router.route('/users/:user_id')
 	.get(function(req,res){
